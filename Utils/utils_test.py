57c491645f54bc061b94b45fabf933098cba2ec4
“AlexCostra” <1937546455@qq.com>
2025-02-10 02:32:34 +0800

    将 utils 文件夹添加到 Dataset 文件夹

---
+++ Utils/utils_test.py
@@ -0,0 +1,108 @@
+import os
+from itertools import islice
+import torch
+from torch_geometric.data import InMemoryDataset, DataLoader
+from torch_geometric import data as DATA
+from rdkit import Chem
+import networkx as nx
+import numpy as np
+class TestbedDataset(InMemoryDataset):
+    def __init__(self, root='/tmp', dataset='_drug1',
+                 xd=None, xt=None, y=None, xt_featrue=None, transform=None,
+                 pre_transform=None, smile_graph=None,smile_feature=None,id=None):
+        super(TestbedDataset, self).__init__(root, transform, pre_transform)
+        self.dataset = dataset
+        if os.path.isfile(self.processed_paths[0]):
+            print('Pre-processed data found: {}, loading ...'.format(self.processed_paths[0]))
+            self.data, self.slices = torch.load(self.processed_paths[0])
+        else:
+            print('Pre-processed data {} not found, doing pre-processing...'.format(self.processed_paths[0]))
+            self.process(xd, xt, xt_featrue, y, smile_graph,smile_feature,id)
+            self.data, self.slices = torch.load(self.processed_paths[0])
+
+    @property
+    def raw_file_names(self):
+        pass
+    @property
+    def processed_file_names(self):
+        return [self.dataset + '.pt']
+    def download(self):
+        # Download to `self.raw_dir`.
+        pass
+    def _download(self):
+        pass
+    def _process(self):
+        if not os.path.exists(self.processed_dir):
+            os.makedirs(self.processed_dir)
+    def get_cell_feature(self, cellId, cell_features):
+        for row in islice(cell_features, 0, None):
+            if cellId in row[0]:
+                return row[1:]
+        return False
+    def process(self, xd, xt, xt_featrue, y, smile_graph,smile_features,id):
+        assert (len(xd) == len(xt) and len(xt) == len(y)), "The three lists must be the same length!"
+        data_list = []
+        data_len = len(xd)
+        for i in range(data_len):
+            smiles = xd[i]
+            target = xt[i]
+            labels = y[i]
+            c_size, features, edge_index = smile_graph[smiles]
+            lens=id[smiles]
+            GCNData = DATA.Data(x=torch.Tensor(features),
+                                edge_index=torch.LongTensor(edge_index).transpose(1, 0),
+                                y=torch.Tensor([labels]))
+            smile_f=smile_features[smiles]
+            cell = self.get_cell_feature(target, xt_featrue)
+            new_cell = []
+            for n in cell:
+                new_cell.append(float(n))
+            GCNData.cell = torch.FloatTensor([new_cell])
+            GCNData.smile2vec_feature = torch.FloatTensor(smile_f)
+            GCNData.__setitem__('c_size', torch.LongTensor([c_size]))
+            GCNData.lens=torch.IntTensor([lens])
+            data_list.append(GCNData)
+        if self.pre_filter is not None:
+            data_list = [data for data in data_list if self.pre_filter(data)]
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(data) for data in data_list]
+        data, slices = self.collate(data_list)
+        torch.save((data, slices), self.processed_paths[0])
+def get_cell_feature(cellId, cell_features):
+    for row in islice(cell_features, 0, None):
+        if row[0] == cellId:
+            return row[1: ]
+
+def atom_features(atom):
+    return np.array(one_of_k_encoding_unk(atom.GetSymbol(),
+                                          ['C', 'N', 'O', 'S', 'F', 'Si', 'P', 'Cl', 'Br', 'Mg', 'Na', 'Ca', 'Fe', 'As',
+                                           'Al', 'I', 'B', 'V', 'K', 'Tl', 'Yb', 'Sb', 'Sn', 'Ag', 'Pd', 'Co', 'Se',
+                                           'Ti', 'Zn', 'H', 'Li', 'Ge', 'Cu', 'Au', 'Ni', 'Cd', 'In', 'Mn', 'Zr', 'Cr',
+                                           'Pt', 'Hg', 'Pb', 'Unknown']) +
+                    one_of_k_encoding(atom.GetDegree(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) +
+                    one_of_k_encoding_unk(atom.GetTotalNumHs(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) +
+                    one_of_k_encoding_unk(atom.GetImplicitValence(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) +
+                    [atom.GetIsAromatic()])
+def one_of_k_encoding(x, allowable_set):
+    if x not in allowable_set:
+        raise Exception("input {0} not in allowable set{1}:".format(x, allowable_set))
+    return list(map(lambda s: x == s, allowable_set))
+def one_of_k_encoding_unk(x, allowable_set):
+    if x not in allowable_set:
+        x = allowable_set[-1]
+    return list(map(lambda s: x == s, allowable_set))
+def smile_to_graph(smile):
+    mol = Chem.MolFromSmiles(smile)
+    c_size = mol.GetNumAtoms()
+    features = []
+    for atom in mol.GetAtoms():
+        feature = atom_features(atom)
+        features.append(feature / sum(feature))
+    edges = []
+    for bond in mol.GetBonds():
+        edges.append([bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()])
+    g = nx.Graph(edges).to_directed()
+    edge_index = []
+    for e1, e2 in g.edges:
+        edge_index.append([e1, e2])
+    return c_size, features, edge_index
